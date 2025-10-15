e4928619db3e732bcaf1ff3983df49959604f85c
minhaz <mdminhaz798@gmail.com>
2025-10-15 20:49:45 +0600

    com

--- practice_12_october/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ practice_12_october/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -133,7 +133,7 @@ namespace practice_12_october.Areas.Identity.Pages.Account
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
-                if (result.Succeeded)
+                if (result.Succeeded)    
                 {
                     _logger.LogInformation("User created a new account with password.");
 
